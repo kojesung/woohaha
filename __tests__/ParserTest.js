54dbcc8c93c2aa2b06deff8a90fa1306fab7a9bb
kojesung <js95112345@gmail.com>
2026-01-09 15:10:24 +0900

    fix:(ParserTest): 테스트 코드 괄호 위치 수정

--- __tests__/ParserTest.js
+++ __tests__/ParserTest.js
@@ -7,21 +7,21 @@ describe("Parser class test", () => {
     expect(selectedFunction).toBe(1);
   });
   test("1~4, Q 기능 입력 파싱 에러 반환 테스트", () => {
-    expect(() => Parser.functionMenuParser("k").toThrow("[ERROR]"));
+    expect(() => Parser.functionMenuParser("k")).toThrow("[ERROR]");
   });
   test("출석 확인 닉네임 입력 테스트", () => {
     const nickname = Parser.attendanceCheckNicknameParser("짱수", ["짱수", "쿠키", "빙티"], student);
     expect(nickname).toBe("짱수");
   });
   test("출석 확인 닉네임 입력 에러 테스트", () => {
-    Parser.attendanceCheckNicknameParser("짱수", ["쿠키", "빙티"], student).toThrow("[ERROR]");
+    expect(() => Parser.attendanceCheckNicknameParser("짱수", ["쿠키", "빙티"], student)).toThrow("[ERROR]");
   });
   test("출석 확인 시간 입력 테스트", () => {
     const time = Parser.attendanceTimeParser("10:30");
     expect(time).toEqual({ hour: 10, minute: 30 });
   });
   test("출석 확인 시간 입력 에러 테스트", () => {
-    expect(() => Parser.attendanceTimeParser("23:30").toThrow("[ERROR]"));
+    expect(() => Parser.attendanceTimeParser("23:30")).toThrow("[ERROR]");
   });
   test("입력한 닉네임이 크루원에 속했는지", () => {
     const nickname = Parser.attendanceNormalNicknameParser("짱수", ["짱수", "쿠키", "빙티"]);
