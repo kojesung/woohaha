91c22792482330e0f9a06000fea9822834393131
kojesung <js95112345@gmail.com>
2026-01-09 15:03:59 +0900

    feat(utils): 크루 닉네임 배열 및 크루원별 출석 기록 반환 유틸 함수 생성

--- src/utils/readAttendanceFile.js
+++ src/utils/readAttendanceFile.js
@@ -4,5 +4,30 @@ export const readAttendanceFile = async () => {
   const content = await fs.promises.readFile("public/attendances.csv", "utf-8");
   const lines = content.trim().split("\n");
   const rows = lines.slice(1, -1).map((line) => line.trim());
-  return rows;
+  const crews = getCrewList(rows);
+  const attendanceMap = getAttendanceRecordPerCrew(rows);
+  return { crews, attendanceMap };
+};
+
+const getCrewList = (rows) => {
+  let crews = [];
+  rows.forEach((row) => {
+    const [nickname, _] = row.split(",");
+    crews.push(nickname);
+  });
+  return [...new Set(crews)];
+};
+
+const getAttendanceRecordPerCrew = (rows) => {
+  const studentMap = new Map();
+  rows.forEach((row) => {
+    const [nickname, dateTime] = row.split(",");
+    const [date, time] = dateTime.split(" ");
+    if (!studentMap.get(nickname)) {
+      studentMap.set(nickname, new Map([[new Date(date), time]]));
+    } else {
+      studentMap.get(nickname).set(new Date(date), time);
+    }
+  });
+  return studentMap;
 };
